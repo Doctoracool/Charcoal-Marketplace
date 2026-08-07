54c8faf9feb2c85a4ae635e22d52e3c46ef2921f
Doctoracool <awwalbalhassan0@gmail.com>
2026-08-07 12:40:53 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 const requiredEnv = [
   "DB_HOST",
+  "DB_PORT",
   "DB_USER",
   "DB_PASSWORD",
   "DB_NAME"
@@ -25,9 +26,11 @@ requiredEnv.forEach((key) => {
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -48,4 +51,4 @@ db.getConnection((err, connection) => {
   connection.release();
 });
 
-module.exports = pool;
+module.exports = db;
